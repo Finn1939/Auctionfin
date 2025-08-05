2bbfd180518b43c362ebcac63d699dc86ef41bde
Finn1939 <serafingo19@gmail.com>
2025-08-06 06:01:59 +0800

    Update start.sh

--- discord-auction-bot/start.sh
+++ discord-auction-bot/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Start Discord bot in background
-python bot.py &
-
-# Start web server in foreground
-uvicorn bot:app --host 0.0.0.0 --port $PORT
+# Start both services in the same Python process
+python bot.py
